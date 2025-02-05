001b733fee1784fb86927ba302d56d76222c2a42
sivasaigannamaneni <sivagannamaneni9@gmail.com>
2025-02-05 15:47:12 +0530

    Day4

---
+++ wipro_java_project/src/com/wipro/java/collection/MapImplementation.java
@@ -0,0 +1,33 @@
+package com.wipro.java.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapImplementation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Map<Integer,String> map=new HashMap<>();
+		map.put(1, "Apple");
+		map.put(2, "Mango");
+		map.put(3, "Banana");
+		
+		System.out.println("Key 1: "+map.get(1));
+		System.out.println("Key 1: "+map.get(2));
+		System.out.println("Key 1: "+map.get(3));
+		
+		if(map.containsKey(3)) {
+			System.out.println("Key 3b exists with value: "+map.get(3));
+		}
+		System.out.println("Iteration through map:");
+		for(Map.Entry<Integer, String> entry: map.entrySet()) {
+			System.out.println(entry.getKey() + ": "+entry.getValue());
+		}
+		map.remove(2);
+		System.out.println("Map After Removal:");
+		for(Map.Entry<Integer, String> entry: map.entrySet()) {
+			System.out.println(entry.getKey() + ": "+entry.getValue());
+		}
+	}
+
+}
